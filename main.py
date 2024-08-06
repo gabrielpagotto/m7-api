33cad3c0fc033396193fc1ee2c44fb79e535d182
Gabriel Pagotto <gabriel.batista@tradexsolutions.com.br>
2024-08-06 12:03:29 -0300

    Update FastAPI title

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from src.feature.riot.handler import router as riot_router
 
 database.Base.metadata.create_all(bind=database.engine)
 
-app = FastAPI()
+app = FastAPI(title="M7 Academy")
 app.add_middleware(
     CORSMiddleware, allow_credentials=True, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
 )
